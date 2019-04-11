b25d1077e5613723c329584795a0b274427d7ca5
peter@macmo <pesegal@gmail.com>
2019-04-10 18:00:58 -0700

    create task update input type

--- src/resolvers/types/TaskInput.ts
+++ src/resolvers/types/TaskInput.ts
@@ -4,7 +4,7 @@ import { MaxLength, IsUUID, IsInt, IsArray } from "class-validator";
 import { string } from "joi";
 
 
-@InputType({ description: "Create Task" })
+@InputType({ description: "Input format for creating a new task" })
 export class CreateTaskInput implements Partial<Task> {
     @Field({ description: "Name of the task"})
     @MaxLength(120)
@@ -24,5 +24,30 @@ export class CreateTaskInput implements Partial<Task> {
     @Field(type => [ID], { nullable: true, description: "List of project Id's that are assigned to this task." })
     @IsArray()
     projectIds!: string[]
+}
 
+@InputType({ description: "Input format for updating a task." })
+export class UpdateTaskInput implements Partial<Task> {
+    @Field({ description: "Task's ID" })
+    @IsUUID()
+    id!: string
+
+    @Field({ nullable:true, description: "Name of the task"})
+    @MaxLength(120)
+    name!: string
+
+    @Field({ nullable: true, description: "Notes attached to the task itself" })
+    notes!: string
+
+    @Field({ nullable: true, description: "List Id" })
+    @IsUUID()
+    listId!:  string
+
+    @Field({ defaultValue: 0, nullable: true, description: "Initial position in the list. Defaults to 0." })
+    @IsInt()
+    columnPos!: number
+
+    @Field(type => [ID], { nullable: true, description: "List of project Id's that are assigned to this task." })
+    @IsArray()
+    projectIds!: string[]
 }
